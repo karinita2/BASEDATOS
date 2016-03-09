0c63361d5a569d332b11c7d1ef98e100a428eeba
fabriciovz <fabribraguev@gmail.com>
2016-03-08 21:23:18 -0430

    Instalacion de Flash Laracast para la publicacion de mensajes

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
          * Personal Providers...
          */
         Collective\Html\HtmlServiceProvider::class,
+        Laracasts\Flash\FlashServiceProvider::class,
 
     ],
 
@@ -209,6 +210,7 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Flash' => Laracasts\Flash\Flash::class,
     ],
 
 ];
